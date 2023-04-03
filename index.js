ffda4fe9fd78fef95e423af49e62234071bac791
HoangTB <tbhoang95@gmail.com>
2023-04-03 08:41:14 +0700

    update file

--- index.js
+++ index.js
@@ -25,11 +25,37 @@ function ex4(){
     const b = Number(prompt("Nhập số nguyên b"));
     const c = Number(prompt("Nhập số nguyên c"));
     const max = a ;
-    if (b > max){
-        alert(b + " Là số nguyên có giá trị lớn nhất" );
-    } if (c > max){
-        alert(c + " Là số nguyên có giá trị lớn nhất" ); 
+    if (a > b && a > c){
+        alert(a + " Là số nguyên có giá trị lớn nhất" );
+    } else if (b > a && b > c){
+        alert(b + " Là số nguyên có giá trị lớn nhất" ); 
 
-    } else alert(a + " Là số nguyên có giá trị lớn nhất" );
+    } else alert(c + " Là số nguyên có giá trị lớn nhất" );
+}
+
+
+function ex5(){
+    const a = Number(prompt("Nhập điểm bài kiểm tra"));
+    const b = Number(prompt("Nhập điểm thi giữa kỳ"));
+    const c = Number(prompt("Nhập điểm thi cuối kỳ"));
+    if ((a + b + c)/3 >= 8){
+        alert(" Xếp hạng giỏi");
+    } else if ((a + b + c)/3 >= 7){
+        alert( " Xếp hạng khá" ); 
+
+    } else alert(" Xếp hạng trung bình" );
+}
+
+
+function ex6(){
+    const n = Number(prompt("Nhập doanh thu năm nay"));
+    if (n ==  100000000){
+        alert("Hoa hồng của bạn là: " + n*0.05);
+    } else if ( n == 110000000){
+        alert( " Hoa hồng của bạn là: " + n*0.07); 
+
+    } else if (n > 110000000){
+        alert( " Hoa hồng của bạn là: " + n*0.1); 
+    }else alert( "Không đủ chỉ tiêu");
 }
 
